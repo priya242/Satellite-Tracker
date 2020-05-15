9a6c99016438df4cd49a5e27ec41429228e515c7
PRIYANKA Pandey <pandey@pdx.edu>
2020-05-15 15:58:34 -0700

    First commit

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer  from "./components/Footer"
+
+function App(){
+  
+    return (
+      // <h1>Hello {firstname +" "+lastname}</h1>
+      //hr - horizontal rule
+       <div>
+           <h2>This is header</h2>
+           <h2>This is main</h2>
+           <h2>This is footer</h2>
+           
+           </div>    
+    )
+}
+
+export default App
---
+++ my-app/src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react"
+function Main(){
+    return(
+        <main>Main Content</main>
+    )
+}
+export default Main
