51dc2c4140fd17e5ede4c4e1714bbf28f5f493f1
PRIYANKA Pandey <pandey@pdx.edu>
2020-05-17 20:34:56 -0700

    video added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,51 @@
-import React from "react"
-import Header from "./components/Header"
-import Main from "./components/Main"
-import Footer  from "./components/Footer"
+import React from "react";
+import './style.css';
+//import { render } from "react-dom";
 
-function App(){
-  
-    return (
-      // <h1>Hello {firstname +" "+lastname}</h1>
-      //hr - horizontal rule
-       <div>
-           <h2>This is header</h2>
-           <h2>This is main</h2>
-           <h2>This is footer</h2>
+//import Header from "./components/Header"
+//import Main from "./components/Main"
+//import Footer  from "./components/Footer"
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            nasa : [],
+            isLoaded : false,
+        };
+    }
+    
+    fetchData(url){
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            return data.collection.items[1].href;
            
-           </div>    
-    )
+        })
+        .then((data) => {
+            this.setState({
+                nasa : data,
+                isLoaded: true,
+            });
+        })
+      // .catch((error) = console.log(error));
+    }
+    componentDidMount() {
+    const url = 'https://images-api.nasa.gov/asset/ISS%204K%20Crew%20Earth%20Observations';
+    this.fetchData(url);
+} 
+render() {
+    const { nasa,isLoaded } = this.state;
+    return (
+        <React.Fragment>
+            <h1>Satellite Tracker</h1>
+            {!isLoaded && <p>Loading...</p>}
+
+            <video autoPlay controls muted loop
+                 src = {nasa} type = "video/mp4" />                             
+             </React.Fragment>     
+      )
 }
 
+}
 export default App
