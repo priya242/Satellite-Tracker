0212143128943e566bbed4c514ee70327be26883
deepa-varghese-88 <dv8@pdx.edu>
2020-05-31 02:45:33 -0700

    highcharts styling for world map added

--- my-app/src/components/nevents/NEworldmap.js
+++ my-app/src/components/nevents/NEworldmap.js
@@ -14,8 +14,23 @@ class NEworldmap extends React.Component {
     const map = mapDataWorld;
 
     const mapOptions = {
+      chart: {
+        backgroundColor: {
+          linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+          stops: [
+            [0, "#2a2a2b"],
+            [1, "#3e3e40"],
+          ],
+        },
+        plotBorderColor: "#606063",
+      },
       title: {
         text: "",
+        // style: {
+        //   color: "#E0E0E3",
+        //   textTransform: "uppercase",
+        //   fontSize: "20px",
+        // },
       },
       subtitle: {
         text: "",
@@ -24,6 +39,10 @@ class NEworldmap extends React.Component {
       tooltip: {
         pointFormat:
           "{point.title}<br>" + "Lat: {point.lat} " + "Lon: {point.lon}",
+        backgroundColor: "rgba(0, 0, 0, 0.85)",
+        style: {
+          color: "#F0F0F0",
+        },
       },
       // colorAxis: {
       //   min: 0,
@@ -45,14 +64,21 @@ class NEworldmap extends React.Component {
           name: "Basemap",
           mapData: map,
           name: "",
+          borderColor: "#A0A0A0",
+          nullColor: "rgba(200, 200, 200, 0.3)",
           showInLegend: false,
         },
         {
           type: "mappoint",
           name: "Event",
+          color: "#90ee7e",
           dataLabels: {
             enabled: true,
             format: "{point.type}",
+            color: "#F0F0F3",
+          },
+          marker: {
+            lineColor: "#333",
           },
           data: data,
           showInLegend: false,
