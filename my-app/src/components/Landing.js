d84617842671e5d2d0c2708ed43b4df4e91c3ba9
PRIYANKA Pandey <pandey@pdx.edu>
2020-06-03 22:36:28 -0700

    landing page ready.... phew

--- my-app/src/components/Landing.js
+++ my-app/src/components/Landing.js
@@ -1,22 +1,21 @@
 import React from "react";
-
+import Slider from "./SliderImage";
 class Landing extends React.Component {
   constructor() {
     super();
     this.state = {
       imgtags: [],
+      imagedate:"",
     };
   }
 
   componentDidMount() {
-    const NASA_API_KEY = encodeURIComponent(process.env.REACT_APP_NE_API_KEY);
+    //const NASA_API_KEY = process.env.REACT_APP_NASA_API_KEY;
     const latest = new Date();
     latest.setDate(latest.getDate() - 1);
-
     function pad(s) {
       return s < 10 ? "0" + s : s;
     }
-
     const latest_string = [
       latest.getFullYear(),
       pad(latest.getMonth() + 1),
@@ -28,53 +27,57 @@ class Landing extends React.Component {
       pad(latest.getMonth() + 1),
       pad(latest.getDate()),
     ].join("/");
-
+    console.log(image_dates[0])
     let images = [];
-    // let l_imgtags = [];
-
-    console.log("Landing");
     fetch(
       `https://api.nasa.gov/EPIC/api/natural/date/${encodeURIComponent(
         latest_string
-      )}?api_key=${encodeURIComponent(NASA_API_KEY)}`
+      )}?api_key=twk4KeZwwqGPWJFY9ksFShKEyoDGimDlJAjwDH0z`
     )
       .then((response) => response.json())
       .then((data) => {
         for (let d of data) {
+          //console.log(d)
           images.push(d.image);
         }
       })
       .then(() => {
-        let l_imgtags = images.map((i) => (
-          <img
-            src={
-              "https://epic.gsfc.nasa.gov/archive/natural/" +
-              image_dates +
-              "/png/" +
-              i +
-              ".png"
-            }
-            alt=""
-          />
-        ));
+        let l_imgtags = images.map(
+          (i) =>
+            "https://epic.gsfc.nasa.gov/archive/natural/" +
+            image_dates +
+            "/png/" +
+            i +
+            ".png"
+        );
+        
         return l_imgtags;
       })
       .then((l_imgtags) => {
-        console.log("inside fetch chain");
-        this.setState({ imgtags: l_imgtags });
-        console.log(this.state.imgtags);
+        this.setState({
+          imgtags: l_imgtags,
+          imagedate: image_dates,
+        });
       })
       .catch((error) => {
         console.log("Request failed: ", error);
       });
+  
   }
-
   render() {
+
     return (
-      <div>
-        LANDING:
-        <br />
-        {this.state.imgtags}
+      <div className="landing">
+        <div className="landing-container">
+          <div className="lgrid-item1">
+            <Slider images={this.state.imgtags} />
+            <p>
+              [This image was taken by NASA's EPIC camera onboard the NOAA
+              DSCOVR on {this.state.imagedate}]
+            </p>
+          </div>
+          <div className="lgrid-item2">Satellite Tracker</div>
+        </div>
       </div>
     );
   }
